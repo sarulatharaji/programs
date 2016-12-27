3084bd6d8fa9bd6e27a957432ba5b4d10dbf115e
sarulatharaji <sarulatha121@gmail.com>
2016-12-27 14:44:27 +0530

    Add files via upload

---
+++ powers.c
@@ -0,0 +1,11 @@
+main()
+{
+int a,n,powers=0;
+printf("enter the number:");
+scanf("%d",&a);
+printf("enter the power:");
+scanf("%d",&n);
+powers=pow(a,n);
+printf("power of %d is....%d",a,powers);
+getch();
+}
