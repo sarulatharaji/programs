3a07510615143e6ea1217ed903feed1ac8477ed7
sarulatharaji <sarulatha121@gmail.com>
2016-12-30 11:22:09 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,22 @@
+package console;
+import java.io.*;
+import java.util.Scanner;
+public class greatest {
+public static void main(String args[])
+{
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the value of a:");
+	int a=s.nextInt();
+	System.out.println("Enter the value of b:");
+	int b=s.nextInt();
+	int i;
+	for(i=a;i<=b;i++)
+	{
+		if(i%2==0)
+			System.out.println("even is"+i);
+		else
+			System.out.println("odd is"+i);
+
+	}
+}
+}
